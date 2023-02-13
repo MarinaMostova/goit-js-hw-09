2ef3d933b07da91361c1c6209d433abc03d394c8
M.Mostova <mmainfo15@gmail.com>
2023-02-13 16:37:00 +0200

    Домашнє завдання 9

---
+++ src/js/03-promises.js
@@ -0,0 +1,40 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let inputDelay = event.currentTarget.delay.valueAsNumber;
+  const inputDelayStep = event.currentTarget.step.valueAsNumber;
+  const inputAmount = event.currentTarget.amount.valueAsNumber;
+
+  for (let i = 1; i <= inputAmount; i += 1) {
+    createPromise(i, inputDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    inputDelay += inputDelayStep;
+  }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
